8c5e028ad6c82c0860f6d0c34b2357582f861030
Edward Bako <edward.bako@gmail.com>
2018-02-25 01:23:47 +0700

    Change server for staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-server '91.240.87.3', port: 22, user: 'deploy', roles: %w(app web db)
+server '46.101.248.95', port: 22, user: 'deploy', roles: %w(app web db)
 
 set :stage, :staging
 set :rails_env, :staging
