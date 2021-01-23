ed6dce867f6da8674ef0e90d19d8714a97d3b09b
Edward Bako <edward.bako@gmail.com>
2021-01-24 00:43:13 +0700

    Add Specification functionality

--- app/models/specification.rb
+++ app/models/specification.rb
@@ -16,4 +16,103 @@
 #   * Long positions swap
 
 class Specification < ApplicationRecord
+
+  class Error < StandardError; end
+  class NoDataError < Error; end
+
+  def pips(lot)
+    (lot_size * lot * point) / rate_for_base
+  end
+
+  def point
+    10**(-precision).to_f
+  end
+
+  def ask
+    get(:ask).to_f
+  end
+
+  def bid
+    get(:bid).to_f
+  end
+
+  def spread
+    ((ask - bid) * 100).to_i
+  end
+
+  def market_prices
+    {
+      bid: bid,
+      ask: ask,
+      spread: spread
+    }
+  end
+
+  def stoploss_cost(lot)
+    pips(lot) * stoploss_level
+  end
+
+  def lot_by_risk(amount)
+    lot = (amount * rate_for_base) / (stoploss_level * point * lot_size)
+    lot.round(lot_size_step_digits)
+  end
+
+  def lot_by_margin(margin, kind)
+    lot = margin / (lot_size * open_price_by_kind(kind))
+    lot.round(lot_size_step_digits)
+  end
+
+  def open_price_by_kind(kind)
+    case kind
+    when :buy
+      ask
+    when :sell
+      bid
+    else
+      ask
+    end
+  end
+
+  def close_price_by_kind(kind)
+    case kind
+    when :buy
+      bid
+    when :sell
+      ask
+    else
+      bid
+    end
+  end
+
+  private
+
+  def get(key)
+    response = $redis.get("#{symbol}:#{key}")
+    raise NoDataError, "There is no market data for :#{symbol} symbol." if response.blank?
+    response
+  end
+
+  def rate_for_base
+    if orders_currency == "USD"
+      1
+    else
+      specification_for_base(orders_currency).ask
+    end
+  end
+
+  def specification_for_base(base)
+    sym = "USD#{base}"
+    specification = Specification.find_by(symbol: sym)
+
+    if specification.present?
+      specification
+    else
+      raise NoDataError, "There is no specification for :#{sym} symbol."
+    end
+  end
+
+  def lot_size_step_digits
+    lot_size_step.to_s.split('.')[1].size
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,46 +7,76 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Specification.create(
-                 symbol: :XAUUSD,
-                 precision: 2,
-                 stoploss_level: 100,
-                 lot_size: 100,
-                 margin_currency: "USD",
-                 orders_currency: "USD",
-                 leverage: 200,
-                 minimum_lot_size: 0.01,
-                 maximum_lot_size: 10000.0,
-                 lot_size_step: 0.01,
-                 short_swap: -1.846,
-                 long_swap: -4.61
+  symbol: :XAUUSD,
+  precision: 2,
+  stoploss_level: 100,
+  lot_size: 100,
+  margin_currency: "USD",
+  orders_currency: "USD",
+  leverage: 200,
+  minimum_lot_size: 0.01,
+  maximum_lot_size: 10000.0,
+  lot_size_step: 0.01,
+  short_swap: -1.846,
+  long_swap: -4.61
 )
 
 Specification.create(
-                symbol: :EURUSD,
-                precision: 5,
-                stoploss_level: 30,
-                lot_size: 100000,
-                margin_currency: "EUR",
-                orders_currency: "USD",
-                leverage: 500,
-                minimum_lot_size: 0.01,
-                maximum_lot_size: 10000.0,
-                lot_size_step: 0.01,
-                short_swap: 1.367,
-                long_swap: -8.813
+  symbol: :EURUSD,
+  precision: 5,
+  stoploss_level: 30,
+  lot_size: 100000,
+  margin_currency: "EUR",
+  orders_currency: "USD",
+  leverage: 500,
+  minimum_lot_size: 0.01,
+  maximum_lot_size: 10000.0,
+  lot_size_step: 0.01,
+  short_swap: 1.367,
+  long_swap: -8.813
 )
 
 Specification.create(
-                symbol: :USDRUB,
-                precision: 4,
-                stoploss_level: 50,
-                lot_size: 100000,
-                margin_currency: "USD",
-                orders_currency: "RUB",
-                leverage: 250,
-                minimum_lot_size: 0.01,
-                maximum_lot_size: 10000.0,
-                lot_size_step: 0.01,
-                short_swap: -31.848,
-                long_swap: -186.034
+  symbol: :USDRUB,
+  precision: 4,
+  stoploss_level: 50,
+  lot_size: 100000,
+  margin_currency: "USD",
+  orders_currency: "RUB",
+  leverage: 250,
+  minimum_lot_size: 0.01,
+  maximum_lot_size: 10000.0,
+  lot_size_step: 0.01,
+  short_swap: -31.848,
+  long_swap: -186.034
+)
+
+Specification.create(
+  symbol: :USDCHF,
+  precision: 5,
+  stoploss_level: 30,
+  lot_size: 100000,
+  margin_currency: "USD",
+  orders_currency: "CHF",
+  leverage: 200,
+  minimum_lot_size: 0.01,
+  maximum_lot_size: 10000.0,
+  lot_size_step: 0.01,
+  short_swap: -3.855,
+  long_swap: 0.416
+)
+
+Specification.create(
+  symbol: :BTCUSD,
+  precision: 3,
+  stop_loss_level: 0,
+  lot_size: 1,
+  margin_currency: "USD",
+  orders_currency: "USD",
+  leverage: 100,
+  minimum_lot_size: 0.01,
+  maximum_lot_size: 15.0,
+  lot_size_step: 0.01,
+  short_swap: 1292.267,
+  long_swap: -14212.12
 )
